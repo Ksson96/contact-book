e89cf5e3ab282b9b265dc650779b425aee2db961
Anton Andersson <anton.andersson001@live.se>
2022-03-14 14:00:15 +0000

    Create Person class

---
+++ run.py
@@ -0,0 +1,10 @@
+
+
+class Person:
+    def __init__(self, fname, lname, age, email, phone_number):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        self.email = email
+        self.phone_number = phone_number
+        
