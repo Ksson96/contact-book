453f6d1fd2a4d401ee4239a9cf87f0d934bc5d81
Anton Andersson <anton.andersson001@live.se>
2022-03-14 14:15:44 +0000

    Add ID attribute to Person object.

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
-
-
 class Person:
-    def __init__(self, fname, lname, age, email, phone_number):
+    """Person Object"""
+    def __init__(self, id, fname, lname, age, email, phone_number):
+        """Instance of Person"""
+        self.id
         self.fname = fname
         self.lname = lname
         self.age = age
         self.email = email
         self.phone_number = phone_number
-        
+
+
+print("Welcome to the Contact Book!")
+print("Choose one of the following options by entering a number between 1-5:")
+
+
