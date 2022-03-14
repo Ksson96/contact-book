498a3369355c41e178f84c8bc8d27437396a4d50
Anton Andersson <anton.andersson001@live.se>
2022-03-14 16:16:10 +0000

    Auto increment contact ID for each Person instance

--- run.py
+++ run.py
@@ -1,21 +1,23 @@
 class Person:
     """Person Object"""
-    def __init__(self, contact_id, fname, lname, age, email, phone_number):
+    __CONTACT_ID = 1
+    def __init__(self, fname, lname, age, email, phone_number ,contact_id):
         """Instance of Person"""
-        self.contact_id = contact_id
         self.fname = fname
         self.lname = lname
         self.age = age
         self.email = email
         self.phone_number = phone_number
+        self.contact_id = Person.__CONTACT_ID
+        Person.__CONTACT_ID += 1
 
     def __str__(self):
         return(
+            f'Id: {self.contact_id}'
             f'Name: {self.fname} {self.lname}\n'
             f'Age: {self.age}\n'
             f'Email: {self.email}\n'
             f'Phone: {self.phone_number}\n'
-            f'Id: {self.contact_id}'
         )
     
 
@@ -26,3 +28,17 @@ print("2. Add Contact")
 print("3. Edit Contact")
 print("4. Remove Contact")
 print("5. Exit Contact Book\n")
+user_choice = input("Enter a number between 1-5: ")
+
+    
+if user_choice == "1":
+    print("You chose 1")
+
+elif user_choice == "2":
+    contact_id = 
+    fname = input("First name: ")
+    lname = input("Last name: ")
+    age = input("Age: ")
+    email = input("Email Address: ")
+    phone_number = input("Phone number: ")
+    
