ea64106ead1fdae228b1d20782af66756611ea32
Anton Andersson <anton.andersson001@live.se>
2022-03-14 16:47:20 +0000

    Append new_contact to contacts list on user input

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 class Person:
     """Person Object"""
     __CONTACT_ID = 1
-    def __init__(self, fname, lname, age, email, phone_number ,contact_id):
+
+    def __init__(self, fname, lname, age, email, phone_number):
         """Instance of Person"""
         self.fname = fname
         self.lname = lname
@@ -13,7 +14,7 @@ class Person:
 
     def __str__(self):
         return(
-            f'Id: {self.contact_id}'
+            f'Id: {self.contact_id}\n'
             f'Name: {self.fname} {self.lname}\n'
             f'Age: {self.age}\n'
             f'Email: {self.email}\n'
@@ -21,24 +22,30 @@ class Person:
         )
     
 
+contacts = list()
+
+user_choice = ""
+
 print("Welcome to the Contact Book!")
-print("What would you like to do?\n")
-print("1. View Contact")
-print("2. Add Contact")
-print("3. Edit Contact")
-print("4. Remove Contact")
-print("5. Exit Contact Book\n")
-user_choice = input("Enter a number between 1-5: ")
 
-    
-if user_choice == "1":
-    print("You chose 1")
+while user_choice != "5":
+    print("What would you like to do?\n")
+    print("1. View Contacts")
+    print("2. Add Contact")
+    print("3. Edit Contact")
+    print("4. Remove Contact")
+    print("5. Exit Contact Book\n")
+    user_choice = input("Enter a number between 1-5: ")
 
-elif user_choice == "2":
-    contact_id = 
-    fname = input("First name: ")
-    lname = input("Last name: ")
-    age = input("Age: ")
-    email = input("Email Address: ")
-    phone_number = input("Phone number: ")
+    if user_choice == "1":
+        print("You chose 1")
+
+    elif user_choice == "2":
+        fname = input("First name: ")
+        lname = input("Last name: ")
+        age = input("Age: ")
+        email = input("Email Address: ")
+        phone_number = input("Phone number: ")
+        new_contact = Person(fname, lname, age, email, phone_number)
+        contacts.append(new_contact)
     
