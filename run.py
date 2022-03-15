e03a08129db7c326c27ec5e92b24181cf438242c
Anton Andersson <anton.andersson001@live.se>
2022-03-15 16:45:14 +0000

    Seperate functionality to it's own function

--- run.py
+++ run.py
@@ -24,48 +24,79 @@ class Person:
 
 contacts = list()
 
-user_choice = ""
 
-print("Welcome to the Contact Book!")
+def view_contacts():
+    """View Contacts"""
+    for contact in contacts:
+        print(contact)
 
-while user_choice != "5":
-    print("What would you like to do?\n")
-    print("1. View Contacts")
-    print("2. Add Contact")
-    print("3. Edit Contact")
-    print("4. Remove Contact")
-    print("5. Exit Contact Book\n")
-    user_choice = input("Enter a number between 1-5: ")
 
-    if user_choice == "1":
-        for contact in contacts:
-            print(contact)
+def create_contact():
+    """Create Contact"""
+    fname = input("First name: ")
+    lname = input("Last name: ")
+    age = input("Age: ")
+    email = input("Email Address: ")
+    phone_number = input("Phone number: ")
+    new_contact = Person(fname, lname, age, email, phone_number)
+    contacts.append(new_contact)
 
-    elif user_choice == "2":
-        fname = input("First name: ")
-        lname = input("Last name: ")
-        age = input("Age: ")
-        email = input("Email Address: ")
-        phone_number = input("Phone number: ")
-        new_contact = Person(fname, lname, age, email, phone_number)
-        contacts.append(new_contact)
-    
-    elif user_choice == "3":
-        edit_id = input("Please enter the ID of the contact you'd like to edit\n")
-        for contact in contacts:
-            if int(edit_id) == contact.contact_id:
-                print(f'Current First Name: {contact.fname}\n')
-                contact.fname = input("Enter new name: ")
 
-                print(f'Current Last Name: {contact.lname}\n')
-                contact.lname = input("Enter new name: ")
+def start():
+    """
+    Displays all menu options to the user
+    """
+    user_choice = ""
+    print("Welcome to the Contact Book!")
+    while user_choice != "5":
 
-                print(f'Current Age: {contact.age}\n')
-                contact.age = input("Enter new age: ")
+        print("What would you like to do?\n")
+        print("1. View Contacts")
+        print("2. Add Contact")
+        print("3. Edit Contact")
+        print("4. Remove Contact")
+        print("5. Exit Contact Book\n")
+        user_choice = input("Enter a number between 1-5: ")
 
-                print(f'Current Email: {contact.email}\n')
-                contact.email = input("Enter new email: ")
+        if user_choice == "1":
+            view_contacts()
 
-                print(f'Current Phone Number: {contact.phone_number}\n')
-                contact.phone_number = input("Enter new phone number: ")
-                
+        elif user_choice == "2":
+            create_contact()
+        
+        elif user_choice == "3":
+            edit_contact()
+
+        elif user_choice == "4":
+            delete_contact()
+
+
+def edit_contact():
+    """Edit Contact"""
+    edit_id = input("Please enter the ID of the contact you'd like to edit\n")
+    for contact in contacts:
+        if int(edit_id) == contact.contact_id:
+            print(f'Current First Name: {contact.fname}\n')
+            contact.fname = input("Enter new name: ")
+
+            print(f'Current Last Name: {contact.lname}\n')
+            contact.lname = input("Enter new name: ")
+
+            print(f'Current Age: {contact.age}\n')
+            contact.age = input("Enter new age: ")
+
+            print(f'Current Email: {contact.email}\n')
+            contact.email = input("Enter new email: ")
+
+            print(f'Current Phone Number: {contact.phone_number}\n')
+            contact.phone_number = input("Enter new phone number: ")
+
+
+def delete_contact():
+    """
+    Delete function
+    """
+    print("Placeholder functionality")
+
+
+start()
