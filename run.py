87f48258d043437dfa565cef59f43ff660035207
Anton Andersson <anton.andersson001@live.se>
2022-03-18 15:48:10 +0000

    Move validation from edit_contact method to display_contact method to work across other methods

--- run.py
+++ run.py
@@ -14,6 +14,37 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('contact_book')
 
 
+def start():
+    """
+    Displays all menu options to the user
+    """
+    user_choice = ""
+    print("Welcome to the Contact Book!")
+
+    print("What would you like to do?\n")
+    print("1. View Contacts")
+    print("2. Add Contact")
+    print("3. Edit Contact")
+    print("4. Remove Contact")
+    print("5. Exit Contact Book")
+
+    while user_choice != "5":
+        user_choice = input("\nEnter a number between 1-5: \n")
+        contact_data = SHEET.worksheet('contact_data')
+        if user_choice == "1":
+            view_contacts(contact_data)
+        elif user_choice == "2":
+            new_contact_id = calc_new_contact_id(contact_data)
+            create_contact(contact_data, new_contact_id)
+        elif user_choice == "3":
+            edit_contact(contact_data)
+
+        elif user_choice == "4":
+            delete_contact(contact_data)
+        else:
+            print(f'"{user_choice}" is not a valid option.')
+
+
 def view_contacts(contact_data):
     """View Contacts"""
     print('****--- Displaying All Contacts ---***\n')
@@ -22,16 +53,6 @@ def view_contacts(contact_data):
         for contact_details in contact:
             print(f'{contact_details}: {contact[contact_details]}')
         print('----------\n')
-
-
-def calc_new_contact_id(contact_data):
-    """
-    Convert str list of IDs to int and return biggest number +1
-    """
-    contact_ids = contact_data.col_values(1)
-    del contact_ids[0]
-
-    return max([int(x) for x in contact_ids]) + 1
 
 
 def create_contact(contact_data, new_contact_id):
@@ -47,58 +68,24 @@ def create_contact(contact_data, new_contact_id):
     contact_data.append_row(new_contact)
 
 
-def start():
-    """
-    Displays all menu options to the user
-    """
-    user_choice = ""
-    print("Welcome to the Contact Book!")
-    while user_choice != "5":
-
-        print("What would you like to do?\n")
-        print("1. View Contacts")
-        print("2. Add Contact")
-        print("3. Edit Contact")
-        print("4. Remove Contact")
-        print("5. Exit Contact Book\n")
-        user_choice = input("Enter a number between 1-5: \n")
-
-        contact_data = SHEET.worksheet('contact_data')
-    
-        if user_choice == "1":
-            view_contacts(contact_data)
-
-        elif user_choice == "2":
-            new_contact_id = calc_new_contact_id(contact_data)
-            create_contact(contact_data, new_contact_id)
-
-        elif user_choice == "3":
-            edit_contact(contact_data)
-
-        elif user_choice == "4":
-            delete_contact(contact_data)
-
-
-# def find_contact(contact_data, contact_id):
-#     cell = contact_data.find(contact_id)
-#     return cell
-
-
 def edit_contact(contact_data):
     """Edit Contact"""
     contact_id = input("Please enter the ID of the contact you'd like to edit\n")
-    contacts = contact_data.get_all_records()
-    cell = contact_data.find(contact_id)
-    for contact in contacts:
-        for contact_details in contact:
-            if contact[contact_details] == int(contact_id):
-                fname = input("Enter new name: ")                
-                lname = input("Enter new name: ")                
-                age = input("Enter new age: ")                
-                email = input("Enter new email: ")                
-                phone_number = input("Enter new phone number: ")
-                contact_data.update(f'B{cell.row}:F{cell.row}', [[fname, lname, age, email, phone_number]])
-    print('Contact updated successfully!')
+    while display_contact(contact_data, contact_id):
+        print("---Displaying Contact to Edit---\n")
+        for key, value in display_contact(contact_data, contact_id).items():
+            print(f'{key}: {value}')
+        cell = contact_data.find(contact_id)
+        fname = input("Enter new name: ")
+        lname = input("Enter new name: ")
+        age = input("Enter new age: ")
+        email = input("Enter new email: ")
+        phone_number = input("Enter new phone number: ")
+        contact_data.update(f'B{cell.row}:F{cell.row}', [[fname, lname, age, email, phone_number]])
+        print('\n----Contact updated successfully!----')
+        break
+    else:
+        print("The user you're looking for doesn't seem to exist")
 
 
 def delete_contact(contact_data):
@@ -120,16 +107,18 @@ def display_contact(contact_data, contact_id):
     """
     contacts = contact_data.get_all_records()
     for contact in contacts:
-        if contact['Contact_Id'] == int(contact_id):
-            for key, value in contact.items():
-                print(f'{key}: {value}')
-# cell = contact_data.find(contact_id)
-# contact = contact_data.row_values(cell.row)
-# print(f'Id: {contact[0]} \n')
-# print(f'Name: {contact[1]} {contact[2]} \n')
-# print(f'Age: {contact[3]}\n')
-# print(f'Email: {contact[4]}\n')
-# print(f'Number: {contact[5]}\n')
+        if int(contact_id) == contact['Contact_Id']:
+            return contact
+
+
+def calc_new_contact_id(contact_data):
+    """
+    Convert str list of IDs to int and return biggest number +1
+    """
+    contact_ids = contact_data.col_values(1)
+    del contact_ids[0]
+
+    return max([int(x) for x in contact_ids]) + 1
 
 
 start()
